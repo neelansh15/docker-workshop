b37bdf1633adf8d344bd2e4d76d03909f4b06ebe
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-06-12 02:54:23 +0530

    Fetch from MongoDB

--- main.js
+++ main.js
@@ -1,31 +1,62 @@
-const app = require('express')()
+const app = require("express")();
 
-const PORT = 8080
+const { MongoClient } = require("mongodb");
+const url =
+  "mongodb+srv://test:test@mycluster.prvob.mongodb.net/docker-workshop?retryWrites=true&w=majority";
+
+const PORT = 8000;
 
 const users = [
-    {
-        id: 1,
-        firstname: "Joe",
-        lastname: "who?",
-        profile_image: "somelink.com/keriferojoeg"
-    },
-    {
-        id: 2,
-        firstname: "Jeremy",
-        lastname: "Clarkson",
-        profile_image: "thegrandtour.com/oewifuoewhg"
-    }
-]
+  {
+    id: 1,
+    firstname: "Joe",
+    lastname: "who?",
+    profile_image: "somelink.com/keriferojoeg",
+  },
+  {
+    id: 2,
+    firstname: "Jeremy",
+    lastname: "Clarkson",
+    profile_image: "thegrandtour.com/oewifuoewhg",
+  },
+];
 
-app.get('/', (req, res) => {
-    res.json(users)
-})
+app.get("/", async (req, res) => {
+  try {
+    const client = new MongoClient(url, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+    await client.connect()
+    const collection = client.db().collection("users")
+    const cursor = await collection.find({})
+    let data = []
+    await cursor.forEach((item) => data.push(item))
+    res.send(data)
+  } catch (e) {
+      console.error(e)
+    res.status(500).send({ error: e });
+  }
+});
 
-app.get('/:id', (req, res) => {
-    res.json(users.filter(item => item.id == req.params.id))
-})
+app.get("/:firstname", async (req, res) => {
+    try {
+        const client = new MongoClient(url, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        await client.connect()
+        const collection = client.db().collection("users")
+        const data = await collection.findOne({
+            firstname: req.params.firstname
+        })
+        res.send(data)
+      } catch (e) {
+          console.error(e)
+        res.status(500).send({ error: e });
+      }
+});
 
-app.listen(8080, () => {
-    console.log(`Listening on Port ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
