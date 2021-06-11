6170f301b7b85360fb92b8c52c5e25775f6edbfa
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-06-12 02:55:19 +0530

    remove old data

--- main.js
+++ main.js
@@ -6,55 +6,40 @@ const url =
 
 const PORT = 8000;
 
-const users = [
-  {
-    id: 1,
-    firstname: "Joe",
-    lastname: "who?",
-    profile_image: "somelink.com/keriferojoeg",
-  },
-  {
-    id: 2,
-    firstname: "Jeremy",
-    lastname: "Clarkson",
-    profile_image: "thegrandtour.com/oewifuoewhg",
-  },
-];
-
 app.get("/", async (req, res) => {
   try {
     const client = new MongoClient(url, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
     });
-    await client.connect()
-    const collection = client.db().collection("users")
-    const cursor = await collection.find({})
-    let data = []
-    await cursor.forEach((item) => data.push(item))
-    res.send(data)
+    await client.connect();
+    const collection = client.db().collection("users");
+    const cursor = await collection.find({});
+    let data = [];
+    await cursor.forEach((item) => data.push(item));
+    res.send(data);
   } catch (e) {
-      console.error(e)
+    console.error(e);
     res.status(500).send({ error: e });
   }
 });
 
 app.get("/:firstname", async (req, res) => {
-    try {
-        const client = new MongoClient(url, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-        await client.connect()
-        const collection = client.db().collection("users")
-        const data = await collection.findOne({
-            firstname: req.params.firstname
-        })
-        res.send(data)
-      } catch (e) {
-          console.error(e)
-        res.status(500).send({ error: e });
-      }
+  try {
+    const client = new MongoClient(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    await client.connect();
+    const collection = client.db().collection("users");
+    const data = await collection.findOne({
+      firstname: req.params.firstname,
+    });
+    res.send(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ error: e });
+  }
 });
 
 app.listen(PORT, () => {
